39144b796edb1ca75362f7365815dd7150714cb0
Daniel Power <me@danielpower.ca>
2022-01-26 00:27:51 -0330

    Pull generator creation into functions

--- src/Generator.hs
+++ src/Generator.hs
@@ -5,14 +5,19 @@ import System.Random (StdGen, newStdGen, split, uniformR)
 import System.Random.Shuffle (shuffle')
 
 -- Recursive perumtation generator
-generateInput :: [[Int]] -> StdGen -> Int -> Int -> [[String]]
-generateInput acc gen n 0 = map (map show) acc -- Base case, convert all the Ints to Strings
-generateInput acc gen n k =
-  generateInput
+doGenerateInput :: [[Int]] -> StdGen -> Int -> Int -> [[String]]
+doGenerateInput acc gen n 0 = map (map show) acc -- Base case, convert all the Ints to Strings
+doGenerateInput acc gen n k =
+  doGenerateInput
     (shuffle' [1 .. n] n gen : acc) -- Create a random permutation and append it to the accumulator
     (fst (split gen)) -- Split the generator. ^1
     n
     (k -1)
+
+generateInput :: Int -> Int -> IO [[String]]
+generateInput n k = do
+  gen <- newStdGen
+  return (doGenerateInput [] gen n k)
 
 -- Takes a list of strings and prints each element separated by some separator
 printList :: [String] -> String -> IO ()
@@ -29,31 +34,36 @@ printLists (x : xs) separator = do
   printList x separator
   printLists xs separator
 
-randInts :: [Int] -> StdGen -> Int -> Int -> Int -> [Int]
-randInts acc gen min max 0 = acc
-randInts acc gen min max count = do
+doRandInts :: [Int] -> StdGen -> Int -> Int -> Int -> [Int]
+doRandInts acc gen min max 0 = acc
+doRandInts acc gen min max count = do
   let (value, newGen) = uniformR (min, max) gen
-  randInts (value : acc) newGen min max (count -1)
+  doRandInts (value : acc) newGen min max (count -1)
+
+randInts :: Int -> Int -> Int -> IO [Int]
+randInts min max count = do
+  gen <- newStdGen
+  return (doRandInts [] gen min max count)
 
 main :: IO ()
 main = do
   args <- getArgs -- Command line arguments
-  gen <- newStdGen -- New random number generator
-  let (gen1, gen2) = split gen -- There's probably a better way to handle this
-  let (gen3, gen4) = split gen1
-  let gen5 = split gen2
   let n = read (head args) :: Int -- `head [any]` returns the first item in a list
   let k = read (args !! 1) :: Int -- `[any] !! Integer` returns the nth item in the list
+  hospitalPreferenceLists <- generateInput n k
+  studentPreferenceLists <- generateInput n k
+  hospitalMap <- randInts 1 k n
+  studentMap <- randInts 1 k n
   print n
   print k
   putChar '\n'
-  printLists (generateInput [] gen1 n k) " " -- Preference lists for hospitals
+  printLists hospitalPreferenceLists " " -- Preference lists for hospitals
   putChar '\n'
-  printLists (generateInput [] gen2 n k) " " -- Preference lists for students
+  printLists studentPreferenceLists " " -- Preference lists for students
   putChar '\n'
-  printList (map show (randInts [] gen3 1 k n)) " " -- Hospital to preference list mapping
+  printList (map show hospitalMap) " " -- Hospital to preference list mapping
   putChar '\n'
-  printList (map show (randInts [] gen4 1 k n)) " " -- Student to preference list mapping
+  printList (map show studentMap) " " -- Student to preference list mapping
 
 -- 1: Since Haskell is purely functional, it does not allow side-effects (except inside monads,
 --    but I haven't gotten that far yet). Thus every time gen is used, it gives the same output.
