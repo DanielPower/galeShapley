f69db4d968e60d8e104e0ea8073d7c8f506fb877
Daniel Power <me@danielpower.ca>
2022-02-02 14:32:01 -0330

    Make generator output zero indexed values

--- src/Generator.hs
+++ src/Generator.hs
@@ -9,10 +9,12 @@ doGenerateInput :: [[Int]] -> StdGen -> Int -> Int -> [[Int]]
 doGenerateInput acc gen n 0 = acc -- Base case, convert all the Ints to Strings
 doGenerateInput acc gen n k =
   doGenerateInput
-    (shuffle' [1 .. n] n gen : acc) -- Create a random permutation and append it to the accumulator
-    (fst (split gen)) -- Split the generator. ^1
+    (shuffle' [0 .. (n -1)] n gen1 : acc) -- Create a random permutation and append it to the accumulator
+    gen2
     n
     (k -1)
+  where
+    (gen1, gen2) = split gen -- Footnote 1
 
 generateInput :: Int -> Int -> IO [[Int]]
 generateInput n k = do
@@ -36,9 +38,10 @@ printLists (x : xs) separator = do
 
 doRandInts :: [Int] -> StdGen -> Int -> Int -> Int -> [Int]
 doRandInts acc gen min max 0 = acc
-doRandInts acc gen min max count = do
-  let (value, newGen) = uniformR (min, max) gen
-  doRandInts (value : acc) newGen min max (count -1)
+doRandInts acc gen min max count =
+  doRandInts (value : acc) newGen min max (count - 1)
+  where
+    (value, newGen) = uniformR (min, max) gen
 
 randInts :: Int -> Int -> Int -> IO [Int]
 randInts min max count = do
@@ -61,15 +64,18 @@ printOutput n k h s hi si = do
 main :: IO ()
 main = do
   args <- getArgs -- Command line arguments
-  let n = read (head args) :: Int -- `head [any]` returns the first item in a list
-  let k = read (args !! 1) :: Int -- `[any] !! Integer` returns the nth item in the list
+  let n = read (head args) :: Int -- `head [a]` returns the first item in a list
+  let k =
+        if length args > 1
+          then read (args !! 1) :: Int -- `[a] !! Int` returns the nth item in the list
+          else n
   h <- generateInput n k -- Preference lists for hospitals
   s <- generateInput n k -- Preference lists for students
-  hi <- randInts 1 k n -- Hospital to preference list mapping
-  si <- randInts 1 k n -- Student to preference list mapping
+  hi <- randInts 0 (k - 1) n -- Hospital to preference list mapping
+  si <- randInts 0 (k - 1) n -- Student to preference list mapping
   printOutput n k h s hi si
 
 -- 1: Since Haskell is purely functional, it does not allow side-effects (except inside monads,
 --    but I haven't gotten that far yet). Thus every time gen is used, it gives the same output.
 --    To get unique values, we split the generator before passing it into the next level of recursion.
---    Split takes a generator and returns two new distinct generators (I only use one of them here).
+--    Split takes a generator and returns two new distinct generators.
